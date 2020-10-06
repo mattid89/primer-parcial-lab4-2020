b579a9e022f72705bf36c985216542376801ef39
mattid89 <matydoto@gmail.com>
2020-10-06 00:37:05 -0300

    alta de peliculas con actores

--- src/app/components/pelicula-alta/pelicula-alta.component.ts
+++ src/app/components/pelicula-alta/pelicula-alta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Actor } from 'src/app/models/actor.model';
 import { Pelicula } from 'src/app/models/pelicula.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-pelicula-alta',
@@ -10,7 +11,8 @@ import { Pelicula } from 'src/app/models/pelicula.model';
 })
 export class PeliculaAltaComponent implements OnInit {
 
-  cant_actores: number = 0;
+  submitted: boolean = false;
+  actoresDeLaPeli: Actor[] = [];
   URL_MOVIE_IMG = "https://lh3.googleusercontent.com/proxy/hMAOpDLQDUNHZoX6-BbgeFNGMaTheSFnGfIDOj72CFwXnZo5_reMvLBWW3dS2Bo7N-qDAOXsC6Gt8ZcNaHmXMqgMlIDj48TlEVuA_J1u";
 
   peliculaForm = new FormGroup({
@@ -21,18 +23,42 @@ export class PeliculaAltaComponent implements OnInit {
     cantidad_publico: new FormControl('')
   });
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit() {
   }
 
   seleccionar(actor: Actor) {
-    console.log(actor);
+    const actorYaAgregado = this.actoresDeLaPeli.indexOf(actor);
+    if (actorYaAgregado !== -1) {
+      return;
+    }
+    this.actoresDeLaPeli.push(actor);
   }
 
   onSubmit() {
-    //$('.alert').alert()
-    console.log(this.peliculaForm)
+    this.submitted = true;
+    if (this.peliculaForm.invalid || this.actoresDeLaPeli.length === 0) {
+      return;
+    }
+    const values = this.peliculaForm.value;
+    const peliNueva = new Pelicula(
+      values.id, 
+      values.nombre, 
+      values.tipo, 
+      values.fecha_estreno, 
+      values.cantidad_publico, 
+      'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/9b/Avenger_Endgame_Poster_Oficial.png/revision/latest/top-crop/width/360/height/450?cb=20190326185910&path-prefix=es', 
+      this.actoresDeLaPeli
+    );
+    this.firebaseService.createPelicula(peliNueva)
+    .then( re => {
+      console.log(re);
+      this.peliculaForm.reset();
+      this.actoresDeLaPeli = [];
+      this.submitted = false;
+    })
+    .catch( ex => console.log );
   }
 
 }
